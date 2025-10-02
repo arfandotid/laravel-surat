736c82ec73bcd762a6cf3aa92e65a6f3f123c0ae
arfandotid <arfandotid@gmail.com>
2025-10-02 10:50:54 +0700

    install laravel ui

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,17 @@
 
 use App\Http\Controllers\JenisSuratController;
 use App\Http\Controllers\SuratController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
 
-Route::resource('jenis_surat', JenisSuratController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::resource('jenis_surat', JenisSuratController::class);
+    Route::post('surat/preview', [SuratController::class, 'preview'])->name('surat.preview');
+    Route::resource('surat', SuratController::class);
+});
 
-Route::post('surat/preview', [SuratController::class, 'preview'])->name('surat.preview');
-Route::resource('surat', SuratController::class);
+Auth::routes();
